60910475f24a3d64c55aa0225131918653c2ed32
kiet1618 <ngokiet3207@gmail.com>
2024-03-12 05:45:25 +0700

    Add device factor derivation and update postcss-preset-env version

--- helpers/recoveryFactor.ts
+++ helpers/recoveryFactor.ts
@@ -2,12 +2,13 @@ import { BN, EC, F, H } from '@common'
 import { lagrangeInterpolation } from '@libs/arithmetic'
 import { TA } from '@types'
 import { setRecoveryKey, getRecoveryKey } from '@helpers/metadata'
+import { deriveDeviceFactor } from '@helpers/deviceFactor'
 
 export const deriveRecoveryFactor = async (
     user: string,
     password: string
 ): Promise<TA.Factor> => {
-    const recoveryFactorX = BN.from(H.keccak256(password), 16)
+    const recoveryFactorX = BN.from(H.keccak256(password).slice(2), 16).umod(EC.ORDER)
     const recoveryFactorKey = await getRecoveryKey(user)
 
     return {
@@ -21,13 +22,15 @@ export const constructRecoveryFactor = async ({
     networkFactor,
     password,
 }: TA.ConstructRecoveryFactorRequest): Promise<TA.ConstructRecoveryFactorResponse> => {
-    const privateKey: BN = BN.from(EC.generatePrivateKey(), 16)
+    const deviceFactor: TA.Factor = deriveDeviceFactor(user) as TA.Factor
     const privateFactor: TA.Factor = {
-        x: F.PRIVATE_FACTOR_X,
-        y: privateKey,
+        x: deviceFactor.x,
+        y: deviceFactor.y,
     }
 
-    const recoveryFactorX = BN.from(H.keccak256(password), 16)
+    const recoveryFactorX = BN.from(H.keccak256(password).slice(2), 16).umod(EC.ORDER)
+
+
     const recoveryKey: BN = lagrangeInterpolation(
         [networkFactor, privateFactor],
         recoveryFactorX
